337186fc2333deb387e587226f25b1443dd752a3
Cristianlp2 <99617966+Cristianlp2@users.noreply.github.com>
2022-10-28 23:49:23 -0300

    Evo

--- evolution_rosemary.mk
+++ evolution_rosemary.mk
@@ -21,14 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device tree
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-
+# Common PE configs
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_USES_AOSP_RECOVERY := true
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := cherish_rosemary
+PRODUCT_NAME := evolution_rosemary
 PRODUCT_DEVICE := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
